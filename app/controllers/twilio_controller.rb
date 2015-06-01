9b13198f01391d28f09f6476a687d1e22ffb562a
Wade Novak <wade.novak@gmail.com>
2015-06-01 17:13:05 -0500

    Moves send sms to rake task

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,8 +2,13 @@ require 'twilio-ruby'
 
 class TwilioController < ApplicationController
   def text
-    @client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"],
-                                       ENV["TWILIO_AUTH_TOKEN"]
+    if Rails.env.production?
+      @client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"],
+                                         ENV["TWILIO_AUTH_TOKEN"]
+    else
+      @client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid,
+                                         Rails.application.secrets.twilio_auth_token
+    end
 
     @client.messages.create({
       from: '+17089720240',
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,4 +1,10 @@
 desc "Hit Twilio alert route"
 task :send_sms => :environment do
-  get "twilio/text" => "twilio#text"
+  client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+
+  client.messages.create({
+    from: '+17089720240',
+    to: '+17082143045',
+    body: 'Hello!'
+  })
 end
