e1cb44d85a95fcd0fb7d23b55e80d63a685bf4a5
Wade Novak <wade.novak@gmail.com>
2015-06-03 10:49:18 -0500

    Changes recap image size:

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -3,7 +3,7 @@ class Memory < ActiveRecord::Base
 
   validates :image, presence: true
 
-  has_attached_file :image, styles: { recap: "750x750>", medium: "300x300>", thumb: "100x100>" },
+  has_attached_file :image, styles: { recap: "900x900>", medium: "300x300>", thumb: "100x100>" },
                             default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
